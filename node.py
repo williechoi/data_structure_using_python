5f314110a6c83ca071a503b3a42799974f093c60
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-04-25 12:06:11 +0900

    first commit: node in python

---
+++ node.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        # Compare the new value with the parent node
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    # print the tree
+    def printTree(self):
+        if self.left:
+            self.left.printTree()
+        print(self.data),
+        if self.right:
+            self.right.printTree()
+
+
+# Use the insert method to add nodes
+root = Node(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+
+root.printTree()
