fdfafc7d37342a80d61e0de4b6f4f1d95db50218
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-05-03 15:34:35 +0900

    array in python

---
+++ arrays.py
@@ -0,0 +1,38 @@
+from array import *
+
+
+def print_array(arr):
+    for x in arr:
+        print(x, end=' ')
+    print('\n')
+    print_separating_line()
+
+
+def print_separating_line():
+    print('----------------------------')
+
+
+array1 = array('i', [10, 20, 30, 40, 50])
+print_array(array1)
+
+print(array1[0])
+print(array1[1])
+print_separating_line()
+
+array1.insert(1, 60)
+print_array(array1)
+
+array1.remove(40)
+print_array(array1)
+
+array1.index(30)
+print_separating_line()
+
+try:
+    array1.index(40)
+except ValueError:
+    print('element not found')
+print_separating_line()
+
+array1[2] = 999
+print_array(array1)
