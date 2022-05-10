27dc25fe8d305781d8f59a76a0a7f5e65886b037
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-05-10 14:05:34 +0900

    add graph algorithm

---
+++ graph_algorithm.py
@@ -0,0 +1,47 @@
+import collections
+
+
+class Graph:
+    def __init__(self, gdict=None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+
+
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+
+    visited.add(start)
+    print(start, end=' ')
+    for next_ in graph[start] - visited:
+        dfs(graph, next_, visited)
+
+    return visited
+
+
+def bfs(graph, startnode):
+    seen, queue = set([startnode]), collections.deque([startnode])
+    while queue:
+        vertex = queue.popleft()
+        marked(vertex)
+        for node in graph[vertex]:
+            if node not in seen:
+                seen.add(node)
+                queue.append(node)
+
+
+def marked(n):
+    print(n, end=' ')
+
+
+gdict = {
+   "a": set(["b", "c"]),
+   "b": set(["a", "d"]),
+   "c": set(["a", "d"]),
+   "d": set(["e"]),
+   "e": set(["a"])
+}
+bfs(gdict, "a")
+print('\n')
+dfs(gdict, "a")
