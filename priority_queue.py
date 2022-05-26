dbe5f3e23e74d01afbb7bc53d19ce52511c3cc08
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-05-26 12:20:36 +0900

    add one more comment

--- priority_queue.py
+++ priority_queue.py
@@ -2,10 +2,12 @@
 PriorityQueue
 queue.PriorityQueue uses heapq internally and shares the same time and space
 complexities. The difference is that PriorityQueue is synchronized and provides
-locking semantics to support multiple concurrent producers and consumers
+***locking semantics*** to support multiple concurrent producers and consumers
 
 you might prefer the class-based interface provided by PriorityQueue
 over the function-based interface provided by heapq
+
+Use Priority Queue if you want to use it for multithreading purposes
 """
 
 from queue import PriorityQueue
