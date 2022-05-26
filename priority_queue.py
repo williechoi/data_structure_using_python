a5245db7ca6496d327a2003002494157588542fe
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-05-26 12:19:24 +0900

    python data structure: priority queue

---
+++ priority_queue.py
@@ -0,0 +1,23 @@
+"""
+PriorityQueue
+queue.PriorityQueue uses heapq internally and shares the same time and space
+complexities. The difference is that PriorityQueue is synchronized and provides
+locking semantics to support multiple concurrent producers and consumers
+
+you might prefer the class-based interface provided by PriorityQueue
+over the function-based interface provided by heapq
+"""
+
+from queue import PriorityQueue
+
+
+q = PriorityQueue()
+
+q.put((2, "code"))
+q.put((1, "eat"))
+q.put((3, "sleep"))
+
+
+while not q.empty():
+    next_item = q.get()
+    print(next_item)
