79643d888bc69566c10294255d2848695d5344e0
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-06-03 17:59:43 +0900

    add defaultdict to chain map

--- chainmap.py
+++ chainmap.py
@@ -8,16 +8,20 @@ Insertions, updates, and deletions only affect the first mapping added to the ch
 
 
 from collections import ChainMap
+from collections import defaultdict
+
 
 dict1 = {"one": 1, "two": 2}
 dict2 = {"three": 3, "four": 4}
+dict3 = defaultdict(jay='samsung', may='kairos')
 
-chain = ChainMap(dict1, dict2)
+chain = ChainMap(dict1, dict2, dict3)
 
 print(chain)
 
 print(chain["one"])
 print(chain["three"])
+print(chain["may"])
 try:
     print(chain["missing"])
 except KeyError as err:
